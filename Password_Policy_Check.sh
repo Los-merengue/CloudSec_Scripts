6ea20c4870ee606d81f7fa686487eb0f6b8ffa78
Los-merengue <michaels4uandme@gmail.com>
2023-12-29 00:09:01 +0300

    Updated the Password_Policy_check.sh with doc string

---
+++ Password_Policy_Check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+:'
+
+This is a useful script for obtaining the Password Policy configuration
+
+Obtaining this information is useful because it will give an audit of how 
+password are being implemented within the cloud environment.
+
+This is essential to prevent the likes of Bruteforce Attacks.
+
+Author: Emeka Michael Nzeopara
+
+'
