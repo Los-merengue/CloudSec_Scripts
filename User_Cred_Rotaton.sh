277028216205d1a989fc903f51d2618d9ce91f9d
Los-merengue <michaels4uandme@gmail.com>
2023-12-28 23:54:23 +0300

    refractor the User_Cred_Rotation.sh script

--- User_Cred_Rotaton.sh
+++ User_Cred_Rotaton.sh
@@ -11,3 +11,10 @@ often the rotation of password and access keys occurs.
 time_map=$(date +"%m-%d-%Y");
 
 echo -e "[*] Starting User Credentials Rotation Script execution at: $time_map\n"
+
+#Dump the AccessKeys Information with Username, Access_Key_Id, and the Creation_Date.
+
+echo -e "[*] Dumping the AccessKeys Information with - Username, Access Key Id and Creation Time"
+
+aws iam list-access-keys --output table --query 'AccessKeyMetadata[*].[UserName,AccessKeyId,CreateDate]'
+
