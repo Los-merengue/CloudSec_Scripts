19c5dceca0ceefcd6665e30f7fefbea91730ef99
Los-merengue <michaels4uandme@gmail.com>
2024-01-06 22:12:00 +0300

    Challenge 1 commit

---
+++ Getting-Started/challenge1.py
@@ -0,0 +1,7 @@
+# Challenge 1
+
+# Write a Python print statement, split over two lines,
+# to print the following output on one line. Use the tip
+# above to help you:
+
+print("This is a gorrilla \n And it is the most dangerous")
---
+++ teni.py
@@ -0,0 +1,14 @@
+# Data Types includes: String, integer, float, turple, list
+# 1. Strings
+
+# A string is anything that is a character. They are created by using a quotation mark ("")
+
+string = "Teni is 14 year old, and he is a dark guy"
+
+print(string)
+
+number = "10"
+
+number1 = 4
+
+print(int(number)+ 3)
