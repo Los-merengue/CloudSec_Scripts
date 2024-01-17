232de532079105e1f2ef3f26448453fe9a9bbcf3
Los-merengue <michaels4uandme@gmail.com>
2024-01-18 01:25:33 +0300

    Modify the script.sh file

--- multi-cloud-file-uploader/script.sh
+++ multi-cloud-file-uploader/script.sh
@@ -48,3 +48,21 @@ echo ${#my_array[2]}
 [[ ${string1} < ${string2} ]] #True if string1 sorts before string2 lexicographically
 
 [[ ${string1} > ${string2} ]] #True if string1 sorts after string2 lexicographically
+
+### Arithmetic Operators
+
+[[ ${arg1} -eq ${arg2} ]] #Returns true if the numbers are equal
+
+[[ ${arg1} -ne ${arg2} ]] #Returns true if the numbers are not equal
+
+[[ ${arg1} -lt ${arg2} ]] #Return true if arg1 is less than arg2
+
+[[ ${arg1} -le ${arg2} ]] #Return true if arg1 is less than or equal arg2
+
+[[ ${arg1} -gt ${arg2} ]] #Returns true if arg1 is greater than arg2
+
+[[ ${arg1} -ge ${arg2} ]] #Returns true if arg1 is greater than or equal arg2
+
+[[ test_case_1 ]] && [[ test_case_2 ]] #And
+
+[[ test_case_1 ]] || [[ test_case_2 ]] #Or
